aed4691b643c4ccb5c591353637cab16267e1874
Aidan <ajsibley6@gmail.com>
2023-07-27 18:50:23 -0400

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+
+const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
