6049792ff0d6e65b64bcc657dfbe44be1391bc26
Aidan <ajsibley6@gmail.com>
2023-07-27 21:15:15 -0400

    buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,35 @@
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
+import Link from 'next/link';
 
 export default function Home() {
   const markdown = ` # Welcome to ajsibley.com 👋
   ---
 
-  This Site is a blank canvas and you are the artist.
-
-**Question** : if you make any idea into a reality, what would it be?  
-**Answer** : Let Us Know 
-# 👇`;
+  Every conversation is a blank canvas and you are the artists.  
+  Why not paint a masterpiece? ✨`;
 
   return (
     <Container maxWidth="sm">
       <ReactMarkdown>{markdown}</ReactMarkdown>
-      <Button
-        variant="contained"
-        color="primary"
-      >
-  Click Me
-</Button>
 
+      {/* Login and Sign Up Buttons */}
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item>
+          <Link href="/login" passHref>
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="/signup" passHref>
+            <Button variant="contained" color="secondary">
+              Sign Up
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
