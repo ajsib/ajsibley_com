7268fd0bce1ddbdab458652d0de6b5739bab7adb
Aidan <ajsibley6@gmail.com>
2023-10-05 07:25:24 -0400

    remove icon s

--- components/home/pages/Communities.tsx
+++ components/home/pages/Communities.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Grid, Paper, Typography, Button, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { FaUsers, FaSearch, FaPollH, FaRegNewspaper } from 'react-icons/fa';
+// import { FaUsers, FaSearch, FaPollH, FaRegNewspaper } from 'react-icons/fa';
 
 const SocialMarketingPage = () => {
   const [expanded, setExpanded] = useState<string | null>(null);
@@ -13,22 +13,22 @@ const SocialMarketingPage = () => {
   const features = [
     {
       title: 'Join Communities',
-      icon: <FaUsers size={30} />,
+      // icon: <FaUsers size={30} />,
       description: 'Find your Vibe! Join communities that resonate with you and engage in meaningful conversations.',
     },
     {
       title: 'Search & Follow Friends',
-      icon: <FaSearch size={30} />,
+      // icon: <FaSearch size={30} />,
       description: 'Easily search for friends and follow their activity. Stay connected, always!',
     },
     {
       title: 'Coordinate Party Locations',
-      icon: <FaPollH size={30} />,
+      // icon: <FaPollH size={30} />,
       description: 'Vote on the hottest party locations and coordinate meetups. Never miss out on the fun!',
     },
     {
       title: 'Club & Community Updates',
-      icon: <FaRegNewspaper size={30} />,
+      // icon: <FaRegNewspaper size={30} />,
       description: 'Stay in the loop with updates from your club or community. Know what&apos;s trending!',
     },
   ];
@@ -48,7 +48,7 @@ const SocialMarketingPage = () => {
           <Grid item xs={12} sm={6} md={4} key={index}>
             <Paper elevation={2} style={{ padding: '20px', minHeight: '200px' }}>
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                {feature.icon}
+                {/* {feature.icon} */}
                 <Typography variant="h4" style={{ marginLeft: '10px' }}>
                   {feature.title}
                 </Typography>
